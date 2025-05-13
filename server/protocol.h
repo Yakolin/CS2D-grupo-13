1b866695f85b961eba5dfc1125314c7605780946
Mateo Riat <mriat@fi.uba.ar>
2025-05-13 15:19:54 -0300

    Agregar métodos de lectura para acciones de jugador en ServerProtocol

--- server/protocol.h
+++ server/protocol.h
@@ -2,6 +2,8 @@
 #define PROTOCOL_H
 
 #include "common/socket.h"
+#include "common/player_types.h"
+#include "player_action.h"
 
 class ServerProtocol
 {
@@ -11,6 +13,23 @@ private:
 public:
     ServerProtocol(Socket &socket);
     ~ServerProtocol();
+
+    void read_player_command();
+
+    MoveRight read_move_right();
+    MoveLeft read_move_left();
+    MoveUp read_move_up();
+    MoveDown read_move_down();
+
+    BuyWeapon read_buy_weapon();
+    BuyAmmo read_buy_ammo();
+    Reload read_reload();
+    Shoot read_shoot();
+    DropWeapon read_drop_weapon();
+
+    PlantBomb read_plant_bomb();
+    DefuseBomb read_defuse_bomb();
+    DropBomb read_drop_bomb();
 };
 
 #endif // !PROTOCOL_H

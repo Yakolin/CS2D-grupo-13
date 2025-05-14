217b9b4f2632299a6b71b9745a2e1d89ebc796a5
Yakolin-Net <ysantamarina@fi.uba.ar>
2025-05-14 16:19:11 -0300

    Update: GameImage.h y GameImage.cpp
    Actualizo esos archivos

--- game/GameImage.h
+++ game/GameImage.h
@@ -7,6 +7,14 @@
 #include "Type.h"
 
 class GameImage {
+    public:
+    void add_player_state(TypesStates::PlayerState&& state) {players.push_back(std::move(state));}
+    void add_object_state(TypesStates::ObjectState&& state) {objects.push_back(std::move(state));}
+    
+    // Hace falta una forma de adquirir esos players y objets
+    private:
+    std::vector<TypesStates::PlayerState> players;
+    std::vector<TypesStates::ObjectState> objects;
 };
 
 

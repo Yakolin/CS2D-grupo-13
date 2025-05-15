6f9d03f88371f67ebf882e5ea7231aefc9c90941
Yaco <sonidoro141@gmail.com>
2025-05-15 19:23:17 -0300

    Update: player_types.h game_image.h
    Remuevo viejos conceptos luego de reunion con compañeros

--- common/game_image.h
+++ common/game_image.h
@@ -1,13 +1,43 @@
 #ifndef GAME_IMAGE_H
 #define GAME_IMAGE_H
 
-class GameImage
-{
+#include <cstdint>
+#include <vector>
+
+#include "player_types.h"
+
+using coordinate_t = std::uint16_t;
+using length_players_images_t = std::uint16_t;
+
+class Position {  // esto puede que este a nivel del server tal vez, ya que
+                  // seguramente quiera usar Position
+public:
+    coordinate_t x;
+    coordinate_t y;
+
+    Position(coordinate_t x, coordinate_t y): x(x), y(y) {}
+    ~Position() = default;
+};
+
+class PlayerImage {
+public:
+    player_id_t player_id;
+    Position position;
+
+    PlayerImage(player_id_t player_id, Position position):
+            player_id(player_id), position(position) {}
+    ~PlayerImage() = default;
+};
+
+class GameImage {
 public:
     GameImage() = default;
     ~GameImage() = default;
-
-    // Add methods to manipulate game images
+    std::vector<PlayerImage> players_images;
+    // Balas en el aire
+    // Bomba
+    // Drops
+    // Sonidos)?
 };
 
-#endif // !GAME_IMAGE_H
+#endif  // !GAME_IMAGE_H
---
+++ common/player_types.h
@@ -0,0 +1,40 @@
+#ifndef PLAYER_TYPES_H
+#define PLAYER_TYPES_H
+
+#include <cstdint>
+
+using player_command_t = std::uint8_t;
+using move_t = std::uint8_t;
+using ammo_t = std::uint16_t;
+using player_id_t = std::uint16_t;
+using weapon_code_t = std::uint8_t;
+using weapon_type_t = std::uint8_t;
+
+enum class PlayerCommandType {
+    MOVE,
+    BUY_WEAPON,
+    BUY_AMMO,
+    RELOAD,
+    SHOOT,
+    DROP_WEAPON,
+    PLANT_BOMB,
+    DEFUSE_BOMB,
+    DROP_BOMB
+};
+
+enum class MoveType {
+    RIGHT,
+    LEFT,
+    UP,
+    DOWN,
+};
+
+enum class WeaponType {
+    PRIMARY,
+    SECONDARY,
+    KNIFE,
+};
+
+enum class WeaponCode { GLOCK, AK47, M3, AWP };
+
+#endif  // !PLAYER_TYPES_H

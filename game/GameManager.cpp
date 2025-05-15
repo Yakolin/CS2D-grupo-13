92febbd4c69a43f031a006e50f80e05ae13242ce
Yaco <sonidoro141@gmail.com>
2025-05-15 20:13:32 -0300

    Update: Polimorfismo de Player en GameManager

--- game/GameManager.cpp
+++ game/GameManager.cpp
@@ -1,52 +1,38 @@
-//  "Copyright 2025 Yaco Santamarina"
 
 #include "GameManager.h"
+
 #include <iostream>
-void GameManager::add_player(std::string&&_nick_name, int id) {
-    Player::Team team = (players.size() % 2 == 0) ? Player::Team::Terrorist : Player::Team::CounterTerrorist;
-    Vector2 position(0,0);
-    players.insert(std::make_pair(id, Player(id,std::move(_nick_name), std::move(position), team)));
-}
 
-void GameManager::handle_player_action(int id ,float x, float y){
-    //Maybe deberia tirar exception si no esta esa id
-    auto it = players.find(id);
-    if (it != players.end()) {
-        it->second.move(std::move(Vector2(x,y)));
-    }
+#include "../common/game_image.h"
+void GameManager::add_player(std::string&& _nick_name, int id) {
+    bool is_ct = (players.size() % 2 == 0) ? false : true;
+    std::shared_ptr<Player> player;
+    Vector2 position(0, 0);
+    if (is_ct)
+        player = std::make_shared<CounterTerrorist>(id, std::move(_nick_name), std::move(position));
+    else
+        player = std::make_shared<CounterTerrorist>(id, std::move(_nick_name), std::move(position));
+    players.insert(std::make_pair(id, player));
 }
-
-void GameManager::start_game(){
-    if (players.size() != 2){
+void GameManager::start_game() {
+    if (players.size() != 2) {
         std::cout << "El juego no tiene suficientes jugadores\n";
         return;
     }
     game_started = true;
 }
 void GameManager::stop_game() {}
+/*
+GameImage GameManager::frame() {
+    // if (!game_started) throw new GameException("E");
 
-GameImage GameManager::generate_game_image(){
-    GameImage game_image;
-    for (auto &pair : players ){
-        TypesStates::player_state_t state = pair.second.player_info();
-        game_image.add_player_state(std::move(state));
-    }
-    //Falta la de los objetos sueltos en el mapa, eso seguro se lo pido al mapa
-    return game_image;
-}
+        1. Actualizar las cosas en el Mapa , como movimiento de las balas , armas q caen
+        2. Chekear colisiones
+        3. Manejar esas colisiones como balas chocando, etc
 
-GameImage GameManager::frame(/*vector <PlayerAction>*/){
-    if (!game_started) throw std::runtime_error(" Imagen generada cuando el juego nisiquiera esta funcionado");
-    /*
-        1. Procesar Todas las PlayerActions , como disparar, moverse, etc.
-        2. Actualizar las cosas en el Mapa , como movimiento de las balas , armas q caen
-        3. Chekear colisiones
-        4. Manejar esas colisiones como balas chocando
-    */
-    handle_player_action(1,1,1);
+    handle_player_action(1, 1, 1);
     return generate_game_image();
 }
-
-GameManager::~GameManager() {
-    players.clear();
-}
+// void process_action(std::unique_ptr<PlayerAction> action) { action.action(this); }
+*/
+GameManager::~GameManager() { players.clear(); }
--- game/GameManager.h
+++ game/GameManager.h
@@ -1,26 +1,35 @@
 #ifndef GAME_MANAGER_H_
 #define GAME_MANAGER_H_
 //  "Copyright 2025 Yaco Santamarina"
-#include "Player.h"
-#include "Map.h"
-#include "GameImage.h"
 #include <map>
+#include <memory>
+#include <string>
+#include <utility>
+
+#include "../common/game_image.h"
+#include "../common/player_command_types.h"
+#include "../server/player_action.h"
+
+#include "Map.h"
+#include "PlayersTypes.h"
 class GameManager {
 private:
-  std::string game_name;
-  std::map<int, Player> players;
-  int round = 0;
-  Map map_game;
-  bool game_started = false;
-  void handle_player_action(int id ,float x, float y);
-  GameImage generate_game_image();
+    std::string game_name;
+    std::map<player_id_t, std::shared_ptr<Player>> players;
+    int round = 0;
+    Map map_game;
+    bool game_started = false;
+    void handle_player_action(int id, float x, float y);
+    GameImage generate_game_image();
+
 public:
-  explicit GameManager(const std::string&_game_name, const std::string& map_name) : game_name(_game_name) , map_game(map_name) {}
-  ~GameManager();
-  void add_player(std::string&&_nick_name, int id);
-  GameImage frame(/*vector <PlayerAction>*/);
-  void start_game();
-  void stop_game();
+    explicit GameManager(const std::string& _game_name, const std::string& map_name):
+            game_name(_game_name), map_game(map_name) {}
+    ~GameManager();
+    void add_player(std::string&& _nick_name, int id);
+    GameImage frame(/*vector <PlayerAction>*/);
+    void start_game();
+    void stop_game();
 };
 
-#endif // GAME_MANAGER_H_
+#endif  // GAME_MANAGER_H_

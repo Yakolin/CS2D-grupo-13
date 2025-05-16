51057b08f57cdd6ba601fc6f5e3adbcd2404142b
Yaco <sonidoro141@gmail.com>
2025-05-16 17:11:41 -0300

    Update: archivo Specials con armas especiales

---
+++ game/Specials.h
@@ -0,0 +1,15 @@
+#ifndef SPECIALS_H_
+#define SPECIALS_H_
+#include "Weapon.h"
+
+
+class Bomb: Weapon {
+    virtual void fire() override;
+};
+
+class Knife: Weapon {
+    virtual void fire() override;
+};
+
+
+#endif  // SPECIALS_H_

062bb6c92ab3df66f8e7bf02a1462028a391d5cf
Yaco <sonidoro141@gmail.com>
2025-05-17 16:23:49 -0300

    Update: Agrego a Physics la clase Rectangle

--- game/Physics.h
+++ game/Physics.h
@@ -1,19 +1,30 @@
 #ifndef GAME_PHYSICS_H_
 #define GAME_PHYSICS_H_
+#include <utility>
 //  "Copyright 2025 Yaco Santamarina"
 class Vector2 {
 public:
-  float x;
-  float y;
-  Vector2(float _x, float _y) : x(_x), y(_y) {}
-  Vector2(Vector2 &&other) = default;
-  Vector2(const Vector2 &) = default;
-  Vector2 operator+(const Vector2 &other);
-  Vector2 &operator+=(const Vector2 &other);
-  Vector2 operator-(const Vector2 &other);
-  Vector2 &operator-=(const Vector2 &other);
-  Vector2 &operator=(Vector2 &&other) = default;
-  Vector2 &operator=(const Vector2 &) = default;
+    float x;
+    float y;
+    Vector2(float _x, float _y): x(_x), y(_y) {}
+    Vector2(Vector2&& other) = default;
+    Vector2(const Vector2&) = default;
+    Vector2 operator+(const Vector2& other);
+    Vector2& operator+=(const Vector2& other);
+    Vector2 operator-(const Vector2& other);
+    Vector2& operator-=(const Vector2& other);
+    Vector2& operator=(Vector2&& other) = default;
+    Vector2& operator=(const Vector2&) = default;
 };
 
-#endif //  GAME_PHYSICS_H_
+class Rectangle {
+private:
+    float width;
+    float height;
+    Vector2 center;
+
+public:
+    Rectangle(float width, float height, Vector2& center):
+            width(width), height(height), center(std::move(center)) {}
+};
+#endif  //  GAME_PHYSICS_H_

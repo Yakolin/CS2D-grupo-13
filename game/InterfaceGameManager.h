0cdee0692555dade47d6ed9ebc3e051d40c77c6e
Yaco <sonidoro141@gmail.com>
2025-05-18 19:15:37 -0300

    Update: agrego funcion de shoot

--- game/InterfaceGameManager.h
+++ game/InterfaceGameManager.h
@@ -8,8 +8,8 @@ class InterfaceGameManager {
 public:
     virtual ~InterfaceGameManager() = default;
     virtual void move(uint16_t player_id, MoveType move_type) = 0;
+    virtual void shoot(uint16_t player_id, float mouse_x, float mouse_y) = 0;
     virtual void reload(uint16_t player_id) = 0;
-    // virtual void shoot(uint16_t player_id, WeaponType weapon_type, ammo_t ammo_count) = 0;
     /* Futuros
     virtual void plant_bomb(uint16_t player_id) = 0;
     virtual void defuse_bomb(uint16_t player_id) = 0;

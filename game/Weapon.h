8d801c1517b8db6695aec7e8bccaad4687ce39d7
Yaco <sonidoro141@gmail.com>
2025-05-18 17:10:44 -0300

    Update: Agrego direccion y posicionn a void fire

--- game/Weapon.h
+++ game/Weapon.h
@@ -1,11 +1,14 @@
 #ifndef WEAPON_H_
 #define WEAPON_H_
 #include "../common/player_command_types.h"
+
+#include "Physics.h"
 class Weapon {
 public:
     WeaponType type;
     ~Weapon() = default;
-    virtual void fire() = 0;  // Recordatorio de este = 0. Significa que es puro
+    virtual void fire(Vector2& position,
+                      Vector2& direction) = 0;  // Recordatorio de este = 0. Significa que es puro
     virtual void reload() = 0;
 
 protected:

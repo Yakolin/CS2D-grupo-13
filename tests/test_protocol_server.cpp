dc52a4646bad360ea0b602f7330a792d99112c7e
Mateo Riat <mriat@fi.uba.ar>
2025-05-18 00:13:12 -0300

    Agregar pruebas para comandos de unirse y listar juegos en ServerProtocol: implementar lógica de pruebas y utilizar mocks para verificar acciones.

--- tests/test_protocol_server.cpp
+++ tests/test_protocol_server.cpp
@@ -108,6 +108,69 @@ TEST(ServerProtocolTest, ReadCreateGameReturnCorrectObject) {
     client_thread.join();
 }
 
+TEST(ServerProtocolTest, ReadJoinGameReturnCorrectObject) {
+    MockGamesMonitor mock_games_monitor;
+    Socket server_socket("9999");
+    player_id_t player_id = 1;
+    std::thread client_thread([]() {
+        Socket client_socket("localhost", "9999");
+        std::string game_name = "mateo game";
+        lobby_command_t command = static_cast<lobby_command_t>(LobbyCommandType::JOIN_GAME);
+        client_socket.sendall(&command, sizeof(command));
+
+        length_name_t length_name = htons(static_cast<length_name_t>(game_name.size()));
+        client_socket.sendall(&length_name, sizeof(length_name_t));
+        client_socket.sendall(game_name.c_str(), game_name.size());
+    });
+
+    Socket server_client = server_socket.accept();
+    ServerProtocol protocol(server_client);
+
+    // Act
+
+    LobbyCommandType command = protocol.read_lobby_command();
+    std::unique_ptr<InterfaceLobbyAction> action = protocol.read_join_game(player_id);
+
+    // Assert
+    ASSERT_EQ(command, LobbyCommandType::JOIN_GAME);
+    EXPECT_CALL(mock_games_monitor, join_game(player_id, "mateo game")).Times(1);
+
+    // Ejecutamos la acción “inyectando” nuestro mock
+    action->action(mock_games_monitor);
+
+    client_thread.join();
+}
+
+TEST(ServerProtocolTest, ReadListGamesReturnCorrectObject) {
+    MockGamesMonitor mock_games_monitor;
+    Socket server_socket("9999");
+    player_id_t player_id = 1;
+    std::thread client_thread([]() {
+        Socket client_socket("localhost", "9999");
+        lobby_command_t command = static_cast<lobby_command_t>(LobbyCommandType::LIST_GAMES);
+        client_socket.sendall(&command, sizeof(command));
+    });
+
+    Socket server_client = server_socket.accept();
+    ServerProtocol protocol(server_client);
+
+    // Act
+    LobbyCommandType command = protocol.read_lobby_command();
+    std::unique_ptr<InterfaceLobbyAction> action = protocol.read_list_games(player_id);
+
+    // Assert
+    ASSERT_EQ(command, LobbyCommandType::LIST_GAMES);
+    MockGamesMonitor mock_monitor;
+    EXPECT_CALL(mock_monitor, list_games())
+            .Times(1)
+            .WillOnce(testing::Return(std::vector<std::string>{"mateo game"}));
+
+    // 8) Ejecutamos la acción (que internamente serializa y envía la lista)
+    action->action(mock_monitor);
+
+    client_thread.join();
+}
+
 
 TEST(ServerProtocolTest, ReadBuyWeapontReturnCorrectObject) {
     // Arrange

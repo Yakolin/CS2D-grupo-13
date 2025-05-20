3481a0b04c9fc1aaa7cb8f8899bd170c630a137d
Mateo Riat <mriat@fi.uba.ar>
2025-05-20 17:39:00 -0300

    borrando build

--- build/_deps/sdl2_mixer-build/CMakeFiles/SDL2_mixer.dir/src/codecs/music_flac.c.o.d
+++
@@ -1,3 +0,0 @@
-_deps/sdl2_mixer-build/CMakeFiles/SDL2_mixer.dir/src/codecs/music_flac.c.o: \
- /mnt/c/Users/mari/OneDrive/Desktop/tp\ final\ taller/CS2D-grupo-13/build/_deps/sdl2_mixer-src/src/codecs/music_flac.c \
- /usr/include/stdc-predef.h

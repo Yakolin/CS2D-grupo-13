9ca4b38993c19f50ed577d840c1311859ae36a54
Yaco <sonidoro141@gmail.com>
2025-05-29 20:29:13 -0300

    Update: CollisionManager se implementa
    Ademas se crea una nueva estructura struct PlayerEntity que representa a un jugador y su posicion

--- server/game/CollisionManager.cpp
+++ server/game/CollisionManager.cpp
@@ -1,1 +1,12 @@
 #include "CollisionManager.h"
+
+#include <iostream>
+
+bool CollisionManager::valid_movement(const Position& actual_position,
+                                      const Position& next_position) {
+    Position destino = actual_position + next_position;
+    // Aca los destinos son distintos porque la matriz esta "invertida" con respecto a los vectores
+    int x = destino.x;
+    int y = destino.y;
+    return walls[y][x] != Wall;
+}
--- server/game/CollisionManager.h
+++ server/game/CollisionManager.h
@@ -8,15 +8,25 @@
 #include "../../common/utility.h"
 
 #include "Colliders.h"
+#include "ICanInteract.h"
+#define Wall '#'
+#define Floor ' '
+typedef struct PlayerEntity {
+    std::shared_ptr<CanInteract>& player;
+    Position position;
+} player_entity_t;
 
 class CollisionManager {
-    std::map<player_id_t, Position>& players;
+    std::vector<std::vector<char>>& walls;
+    std::map<player_id_t, player_entity_t>& players_in_map;
     std::map<player_id_t, std::unique_ptr<Collider>>& damage_colliders;
     // std::vector<Dropped> dropped
 public:
-    explicit CollisionManager(std::map<player_id_t, Position>& players_map,
+    explicit CollisionManager(std::vector<std::vector<char>>& walls,
+                              std::map<player_id_t, player_entity_t>& players_in_map,
                               std::map<player_id_t, std::unique_ptr<Collider>>& damage_colliders):
-            players(players_map), damage_colliders(damage_colliders) {}
+            walls(walls), players_in_map(players_in_map), damage_colliders(damage_colliders) {}
+    bool valid_movement(const Position& actual_position, const Position& next_position);
 };
 
 #endif  // COLLISION_MANAGER_H_

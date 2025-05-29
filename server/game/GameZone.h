276089f7bb4c9a1b267492b572e7f6bc061157fc
Yaco <sonidoro141@gmail.com>
2025-05-29 20:20:21 -0300

    Update: Interfaces GameZone y ICanInteract

---
+++ server/game/GameZone.h
@@ -0,0 +1,13 @@
+#ifndef GAME_ZONE_H_
+#define GAME_ZONE_H_
+
+#include <utility>
+
+#include "../../common/game_image.h"
+#include "../../common/utility.h"
+class GameZone {
+public:
+    virtual void move(player_id_t id, const Position& direction) = 0;
+    virtual ~GameZone() = default;
+};
+#endif  // GAME_ZONE_H_
---
+++ server/game/ICanInteract.h
@@ -0,0 +1,9 @@
+#ifndef INTERFACE_CAN_INTERACT_H_
+#define INTERFACE_CAN_INTERACT_H_
+#include "Weapon.h"
+
+class CanInteract {
+    virtual void damage(uint8_t damage) = 0;
+    // virtual void equip(std::unique_ptr<Weapon>& weapon) = 0;
+};
+#endif  // INTERFACE_CAN_INTERACT_H_

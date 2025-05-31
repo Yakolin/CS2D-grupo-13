f27048a2ef56de6f1dde7b581cba184386307aef
Yaco <sonidoro141@gmail.com>
2025-05-31 13:40:47 -0300

    Update: Mejoro ISpawneableZone

--- server/game/ISpawneableZone.h
+++ server/game/ISpawneableZone.h
@@ -1,8 +1,17 @@
 #ifndef I_SPAWNEABLE_ZONE_H_
 #define I_SPAWNEABLE_ZONE_H_
+#include <memory>
+
 #include "Colliders.h"
 
 class ISpawneableZone {
-    virtual void spawn_collider(Collider& collider, uint8_t times /* Algo mas)?*/) = 0;
+public:
+    typedef struct DamageCollider {
+        uint8_t width;
+        uint8_t distance;
+        Position& direction;
+    } damage_collider_t;
+    virtual void spawn_collider(player_id_t id_spawn, damage_collider_t& wanted) = 0;
+    virtual ~ISpawneableZone() = default;
 };
 #endif  //  I_SPAWNEABLE_ZONE_H_

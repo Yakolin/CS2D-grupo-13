c649bccf85fa26827824e73b0f32f590a494e7ed
Yaco <sonidoro141@gmail.com>
2025-06-02 19:10:58 -0300

    Update: Agrego Knife como Weapon

---
+++ server/game/Specials.cpp
@@ -0,0 +1,11 @@
+#include "Specials.h"
+
+void Knife::set_on_action(ISpawneableZone& spawn, player_id_t id, Position& direction) {
+    ISpawneableZone::damage_collider_t wanted = {
+            2, 2, direction};  // Es muy cercano, asi que 2 esta bien)?
+    spawn.spawn_collider(id, wanted);
+}
+void Knife::reload() { return; }
+WeaponImage Knife::get_weapon_image() {
+    return WeaponImage(code, 0, 0, 0);  // Aca evidentemente no maneja balas el cuchillo
+}
--- server/game/Specials.h
+++ server/game/Specials.h
@@ -6,8 +6,13 @@ class Bomb {
 public:
 };
 
-class Knife {
+class Knife: public Weapon {
 public:
+    explicit Knife(WeaponCode code): Weapon(code) {}
+    virtual void set_on_action(ISpawneableZone& spawn, player_id_t id,
+                               Position& direction) override;
+    virtual void reload() override;
+    virtual WeaponImage get_weapon_image() override;
 };
 
 #endif  // SPECIALS_H_

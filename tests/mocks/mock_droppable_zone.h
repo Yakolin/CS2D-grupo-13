c39f92eb0d48fbb23118b1c59c03913a0c23cea1
Mateo RIat <mriat@fi.uba.ar>
2025-06-04 15:54:08 -0300

    test: empiezo  a armar el ambiente de test para el equipamiento

---
+++ tests/mocks/mock_droppable_zone.h
@@ -0,0 +1,14 @@
+#ifndef MOCK_DROPPABLE_ZONE_H
+#define MOCK_DROPPABLE_ZONE_H
+
+#include <gmock/gmock.h>
+
+#include "../../server/game/IDropeableZone.h"
+
+class MockIDroppableZone: public IDroppableZone {
+public:
+    MOCK_METHOD(void, drop, (const player_id_t& player_id, std::unique_ptr<Weapon>& dropeable),
+                (override));
+};
+
+#endif  // !MOCK_DROPPBLE_ZONE_H

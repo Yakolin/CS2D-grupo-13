4bc439eb8c4d5e360d7ada8bda20d2d97378e25f
Yaco <sonidoro141@gmail.com>
2025-06-05 19:36:45 -0300

    Update: el timer activa el temporizador de bomb

--- server/game/Timer.h
+++ server/game/Timer.h
@@ -9,7 +9,9 @@ class Timer {
 
     int bomb_duration = 15;  // ??? deberia ir aca?
     bool round_started = false;
+    bool bomb_activate = false;
     clock::time_point round_start_time;
+    clock::time_point bomb_start_time;
 
 public:
     Timer() = default;
@@ -18,6 +20,10 @@ public:
         round_start_time = clock::now();
         round_started = true;
     }
+    void bomb_start() {
+        bomb_start_time = clock::now();
+        bomb_activate = true;
+    }
     int get_time_buy() const {
         auto time =
                 std::chrono::duration_cast<std::chrono::seconds>(clock::now() - round_start_time)
@@ -25,10 +31,11 @@ public:
         return std::max(0, buy_duration - static_cast<int>(time));
     }
     int get_time_round() const {
+        int round_real_time = (bomb_activate) ? bomb_duration : round_duration;
+        clock::time_point time_start = (bomb_activate) ? bomb_start_time : round_start_time;
         auto time =
-                std::chrono::duration_cast<std::chrono::seconds>(clock::now() - round_start_time)
-                        .count();
-        return std::max(0, round_duration - static_cast<int>(time));
+                std::chrono::duration_cast<std::chrono::seconds>(clock::now() - time_start).count();
+        return std::max(0, round_real_time - static_cast<int>(time));
     }
     bool is_round_over() { return round_started && get_time_round() == 0; }
     bool is_time_buy() { return get_time_buy() > 0; }

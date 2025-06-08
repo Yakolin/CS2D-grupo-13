23376e8f89c3620cc936501a6d1129ce5958e93e
Mateo RIat <mriat@fi.uba.ar>
2025-06-08 04:44:08 -0300

    fix: remuevo archivos innecesarios

--- tests/test_game/test_equipment/test_equipment.cpp
+++
@@ -1,6 +0,0 @@
-#include <gmock/gmock.h>
-#include <gtest/gtest.h>
-
-#include "../../../server/game/Player/Equipment.h"
-#include "../../mocks/mock_droppable_zone.h"
-#include "../../mocks/mock_spawmeable_zone.h"

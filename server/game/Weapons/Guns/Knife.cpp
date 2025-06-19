0a38c8b027bb6ecfc8366ac825ab3393f7f59278
Mateo RIat <mriat@fi.uba.ar>
2025-06-19 16:52:39 -0300

    fix: el knife no estaba construyendose bien

--- server/game/Weapons/Guns/Knife.cpp
+++ server/game/Weapons/Guns/Knife.cpp
@@ -1,7 +1,7 @@
 #include "Knife.h"
 
 Knife::Knife(GameConfig::weapon_config_t specs):
-        Weapon(WeaponCode::KNIFE, specs.damage, specs.range, specs.width,
+        Weapon(WeaponCode::KNIFE, specs.damage, specs.distance, specs.width,
                std::make_unique<SemiAutomatic>(static_cast<fire_rate_t>(specs.fire_rate))) {}
 
 Knife::~Knife() {}
@@ -10,32 +10,28 @@ damage_t Knife::calculate_damage() { return Weapon::damage; }
 
 bool Knife::set_on_action(ISpawneableZone& spawn, player_id_t id, Position& direction) {
     if (this->fire_mode->can_fire()) {
-        damage_t damage = Weapon::damage;
-        ISpawneableZone::collider_solicitude_t wanted = {specs.width, specs.distance, direction,
-                                                         damage};
+        ISpawneableZone::collider_solicitude_t wanted = {
+                this->width, this->range, direction, [this]() { return this->calculate_damage(); }};
 
         spawn.spawn_collider(id, wanted);
         return true;
     }
-    return false
+    return false;
 }
 
 bool Knife::shoot_burst(ISpawneableZone& spawn, player_id_t id, Position& direction) {
     if (this->fire_mode->can_fire_burst()) {
-        damage_t damage = Weapon::damage;
-        ISpawneableZone::collider_solicitude_t wanted = {specs.width, specs.distance, direction,
-                                                         damage};
+        ISpawneableZone::collider_solicitude_t wanted = {
+                this->width, this->range, direction, [this]() { return this->calculate_damage(); }};
 
         spawn.spawn_collider(id, wanted);
         return true;
     }
-    return false
+    return false;
 }
 
-void Knife::reload() = default;
+void Knife::reload() {};
 
-void Knife::restart() = default;
-
-WeaponImage Knife::get_weapon_image() { return WeaponImage(code, 0, 0, 0); }
+WeaponImage Knife::get_weapon_image() { return WeaponImage(code, 0, 0); }
 
 bool Knife::is_droppable() { return false; }

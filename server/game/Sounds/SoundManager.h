17b4906839af4e8fcf678132f7dd2f5892f3f35e
Yaco <sonidoro141@gmail.com>
2025-06-20 14:51:17 -0300

    Update:SoundManager cambia mapa por vector de pair

--- server/game/Sounds/SoundManager.h
+++ server/game/Sounds/SoundManager.h
@@ -1,19 +1,21 @@
 #ifndef SOUND_MANAGER_H_
 #define SOUND_MANAGER_H_
-#include <map>
 #include <memory>
+#include <vector>
+
+#include "../../../common/utility.h"
 
 #include "Sound.h"
 class SoundManager {
-    std::map<Sound, Position> sounds;
+    std::vector<std::pair<Sound, Position>> sounds;
 
 public:
     SoundManager() = default;
     void emit_sound(const Sound& sound, const Position& pos) {
-        sounds.insert(std::make_pair(sound, pos));
+        sounds.push_back(std::make_pair(sound, pos));
     }
-    std::map<Sound, Position> get_emited_sounds() {
-        std::map<Sound, Position> aux = this->sounds;
+    std::vector<std::pair<Sound, Position>> get_emited_sounds() {
+        std::vector<std::pair<Sound, Position>> aux = this->sounds;
         this->sounds.clear();
         return aux;
     };

b44a9f2983b48ca9f601360ae567e321f527ed2b
Yaco <sonidoro141@gmail.com>
2025-06-20 14:51:50 -0300

    Fix: Modifico ISoundZone

--- server/game/Map/ISoundZone.h
+++ server/game/Map/ISoundZone.h
@@ -4,10 +4,10 @@
 
 #include "../../../common/game_image.h"
 #include "../../../common/utility.h"
-
+#include "../Sounds/Sound.h"
 class ISoundZone {
 public:
-    virtual void want_emit_sound(const player_id_t& id, Sound& sound) = 0;
+    virtual void want_emit_sound(const player_id_t& id, Sound sound) = 0;
     virtual ~ISoundZone() = default;
 };
 

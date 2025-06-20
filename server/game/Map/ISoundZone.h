e74e2c79a053de3861100c6bf6ab086d3cf2047c
Yaco <sonidoro141@gmail.com>
2025-06-19 22:34:17 -0300

    Bugfix con ISoundZone

--- server/game/Map/ISoundZone.h
+++ server/game/Map/ISoundZone.h
@@ -6,8 +6,9 @@
 #include "../../../common/utility.h"
 
 class ISoundZone {
+public:
     virtual void want_emit_sound(const player_id_t& id, SoundType sound) = 0;
     virtual ~ISoundZone() = default;
-}
+};
 
 #endif  // I_SOUND_ZONE_H_

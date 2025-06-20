45ffe01a6420938c6be1e6fc85718d2e69e9c80c
Yaco <sonidoro141@gmail.com>
2025-06-19 22:32:55 -0300

    Agrego ISoundZone

---
+++ server/game/Map/ISoundZone.h
@@ -0,0 +1,13 @@
+#ifndef I_SOUND_ZONE_H_
+#define I_SOUND_ZONE_H_
+#include <utility>
+
+#include "../../../common/game_image.h"
+#include "../../../common/utility.h"
+
+class ISoundZone {
+    virtual void want_emit_sound(const player_id_t& id, SoundType sound) = 0;
+    virtual ~ISoundZone() = default;
+}
+
+#endif  // I_SOUND_ZONE_H_
